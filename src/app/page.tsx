182c76b478008bd39bf695edf1c5342232afd1c0
Harsh6727 <harshsinghnaruka1999@gmail.com>
2025-07-29 18:56:07 +0530

    08-e2b-agentTool-messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,33 @@
+// 1️⃣ Add this directive at the very top (no import!)
 "use client";
 
+import { useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query"; 
+import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
-import { useState } from "react";
+import { toast } from "sonner";
 
 const Page = () => {
-  const [value, setValue] = useState("")
-  
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+  const {data: messages} = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({
+    onSuccess: ()=>{
+      toast.success("Message Created")
+    }
+  }));
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Input value={value} onChange={(e)=>setValue(e.target.value)} />
-      <Button onClick={()=>invoke.mutate({ value: value })}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button onClick={() => createMessage.mutate({ value: value })}>
         Invoke Background job
       </Button>
+      
+      {JSON.stringify(messages, null )}
     </div>
-  )
-}
+  );
+};
 
 export default Page;
