dc6e16a2b3fc0aa90b0a652c0791e658a19b6598
Harsh6727 <harshsinghnaruka1999@gmail.com>
2025-08-24 01:22:06 +0530

    19-Fixes

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -15,6 +15,7 @@ import Link from "next/link";
 import { FileExplorer } from "@/components/file-explorer";
 import { UserControl } from "@/components/user-control";
 import { useAuth } from "@clerk/nextjs";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface Props {
     projectId : string;
@@ -37,16 +38,20 @@ export const ProjectView = ({projectId}: Props) => {
         <div className="h-screen">
             <ResizablePanelGroup direction="horizontal">
                 <ResizablePanel defaultSize={35} minSize={20} className="flex flex-col min-h-0">
+                    <ErrorBoundary fallback={<p>Project Header Error!</p>}>
                     <Suspense fallback={<p>Loading Project...</p>}>
                         <ProjectHeader projectId={projectId} />
                     </Suspense>
-                    <Suspense fallback={<p>Loading messages...</p>}>
-                        <MessageContainer 
-                        projectId={projectId} 
-                        activeFragment = {activeFragment}
-                        setActiveFragment = {setActiveFragment}
-                        />
-                    </Suspense>
+                    </ErrorBoundary>
+                    <ErrorBoundary fallback={<p>Messages container Error!</p>}>
+                        <Suspense fallback={<p>Loading messages...</p>}>
+                            <MessageContainer 
+                            projectId={projectId} 
+                            activeFragment = {activeFragment}
+                            setActiveFragment = {setActiveFragment}
+                            />
+                        </Suspense>
+                    </ErrorBoundary>
                 </ResizablePanel>
                 <ResizableHandle className="hover:bg-primary transition-colors" />
                 <ResizablePanel defaultSize={65} minSize={50}>
